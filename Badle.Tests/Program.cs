40169b825d811d6cdadc1a5c2f8bcce60ac60f35
Millzy <millzyyt08@gmail.com>
2022-03-04 10:19:40 +0000

    tests

---
+++ Badle.Tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using Badle.Wordle;
+using System;
+
+namespace Badle.Tests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            string randomWord = Words.Instance.Answers[random.Next(Words.Instance.Answers.Length)];
+            Console.WriteLine(randomWord);
+        }
+    }
+}
